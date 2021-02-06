87d6ed3786a4f930ca30f3f9bbe2e1327dc57379
Fred Perry <fkischewski99@gmail.com>
2021-02-06 18:55:31 +0100

    inital index.js setup

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const consolidate = require('consolidate')
+
+
+
+let app = express()
+const initRouter = require("./routes/init-router.js")
+
+app.use(express.json)
+app.use(express.urlencoded({extended:false}))
+app.use("/init", initRouter)
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`ZickZackZiege listens on Port ${PORT}`)
+})
+
